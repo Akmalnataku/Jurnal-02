d6e33f38ee44c16684be64a999426006951e36a4
Akmalnataku <71364601+Akmalnataku@users.noreply.github.com>
2020-09-16 11:52:20 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama: Akmal Natakusuma");
+		System.out.println("NIM: 6706202090");
+		System.out.println("Kelas: D3IF-44-02");
+	}
+}
