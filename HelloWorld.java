31210a5ebfa5b013596041f159fd85c2a780da18
Akmalnataku <71364601+Akmalnataku@users.noreply.github.com>
2020-09-16 12:43:22 +0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-	public static void main (String[] args) {
-		System.out.println("Nama: Akmal Natakusuma");
-		System.out.println("NIM: 6706202090");
-		System.out.println("Kelas: D3IF-44-02");
-	}
-}
